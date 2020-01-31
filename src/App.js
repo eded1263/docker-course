f9bc7f727fb00594769fa9593f04de5c32c0c764
edilson <edilson@quicktech.no>
2020-01-31 11:13:44 -0300

    changing the text from the app

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          ofngsofjgiodfjgoisij
+          Running on the branches hehe
         </p>
         <a
           className="App-link"
